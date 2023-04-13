79c9555089988bcd8793cf5bffea85c30342dc80
Siim Tishler <siim.tishlr@gmail.com>
2023-04-13 16:16:07 +0300

    Sqlite test

--- KaartSWEPrj/Kaardirakendus.Designer.vb
+++ KaartSWEPrj/Kaardirakendus.Designer.vb
@@ -22,120 +22,13 @@ Partial Class Kaardirakendus
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.GMapControl1 = New GMap.NET.WindowsForms.GMapControl()
-        Me.btnChoose = New System.Windows.Forms.Button()
-        Me.lblLat = New System.Windows.Forms.Label()
-        Me.lblLongName = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.btnDisplay = New System.Windows.Forms.Button()
-        Me.txtLongName = New System.Windows.Forms.TextBox()
-        Me.txtLat = New System.Windows.Forms.TextBox()
-        Me.btnAll = New System.Windows.Forms.Button()
         Me.btnShowLines = New System.Windows.Forms.Button()
         Me.lBoxLiinid = New System.Windows.Forms.ListBox()
-        Me.lBoxPeatused = New System.Windows.Forms.ListBox()
-        Me.btnAB = New System.Windows.Forms.Button()
-        Me.btnBA = New System.Windows.Forms.Button()
-        Me.tBoxLink = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
-        '
-        'GMapControl1
-        '
-        Me.GMapControl1.Bearing = 0!
-        Me.GMapControl1.CanDragMap = True
-        Me.GMapControl1.EmptyTileColor = System.Drawing.Color.Navy
-        Me.GMapControl1.GrayScaleMode = False
-        Me.GMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow
-        Me.GMapControl1.LevelsKeepInMemory = 5
-        Me.GMapControl1.Location = New System.Drawing.Point(175, 24)
-        Me.GMapControl1.MarkersEnabled = True
-        Me.GMapControl1.MaxZoom = 2
-        Me.GMapControl1.MinZoom = 2
-        Me.GMapControl1.MouseWheelZoomEnabled = True
-        Me.GMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter
-        Me.GMapControl1.Name = "GMapControl1"
-        Me.GMapControl1.NegativeMode = False
-        Me.GMapControl1.PolygonsEnabled = True
-        Me.GMapControl1.RetryLoadTile = 0
-        Me.GMapControl1.RoutesEnabled = True
-        Me.GMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.[Integer]
-        Me.GMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(105, Byte), Integer), CType(CType(225, Byte), Integer))
-        Me.GMapControl1.ShowTileGridLines = False
-        Me.GMapControl1.Size = New System.Drawing.Size(603, 402)
-        Me.GMapControl1.TabIndex = 0
-        Me.GMapControl1.Zoom = 0R
-        '
-        'btnChoose
-        '
-        Me.btnChoose.Location = New System.Drawing.Point(12, 46)
-        Me.btnChoose.Name = "btnChoose"
-        Me.btnChoose.Size = New System.Drawing.Size(75, 23)
-        Me.btnChoose.TabIndex = 1
-        Me.btnChoose.Text = "Name"
-        Me.btnChoose.UseVisualStyleBackColor = True
-        '
-        'lblLat
-        '
-        Me.lblLat.AutoSize = True
-        Me.lblLat.Location = New System.Drawing.Point(12, 129)
-        Me.lblLat.Name = "lblLat"
-        Me.lblLat.Size = New System.Drawing.Size(45, 13)
-        Me.lblLat.TabIndex = 2
-        Me.lblLat.Text = "Latitude"
-        '
-        'lblLongName
-        '
-        Me.lblLongName.AutoSize = True
-        Me.lblLongName.Location = New System.Drawing.Point(9, 84)
-        Me.lblLongName.Name = "lblLongName"
-        Me.lblLongName.Size = New System.Drawing.Size(54, 13)
-        Me.lblLongName.TabIndex = 3
-        Me.lblLongName.Text = "Longitude"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 24)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(66, 13)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "Choose stop"
-        '
-        'btnDisplay
-        '
-        Me.btnDisplay.Location = New System.Drawing.Point(93, 46)
-        Me.btnDisplay.Name = "btnDisplay"
-        Me.btnDisplay.Size = New System.Drawing.Size(75, 23)
-        Me.btnDisplay.TabIndex = 5
-        Me.btnDisplay.Text = "Display"
-        Me.btnDisplay.UseVisualStyleBackColor = True
-        '
-        'txtLongName
-        '
-        Me.txtLongName.Location = New System.Drawing.Point(12, 100)
-        Me.txtLongName.Name = "txtLongName"
-        Me.txtLongName.Size = New System.Drawing.Size(100, 20)
-        Me.txtLongName.TabIndex = 6
-        '
-        'txtLat
-        '
-        Me.txtLat.Location = New System.Drawing.Point(12, 145)
-        Me.txtLat.Name = "txtLat"
-        Me.txtLat.Size = New System.Drawing.Size(100, 20)
-        Me.txtLat.TabIndex = 7
-        '
-        'btnAll
-        '
-        Me.btnAll.Location = New System.Drawing.Point(15, 181)
-        Me.btnAll.Name = "btnAll"
-        Me.btnAll.Size = New System.Drawing.Size(75, 23)
-        Me.btnAll.TabIndex = 8
-        Me.btnAll.Text = "Display All"
-        Me.btnAll.UseVisualStyleBackColor = True
         '
         'btnShowLines
         '
-        Me.btnShowLines.Location = New System.Drawing.Point(784, 24)
+        Me.btnShowLines.Location = New System.Drawing.Point(229, 31)
         Me.btnShowLines.Name = "btnShowLines"
         Me.btnShowLines.Size = New System.Drawing.Size(125, 52)
         Me.btnShowLines.TabIndex = 9
@@ -145,86 +38,25 @@ Partial Class Kaardirakendus
         'lBoxLiinid
         '
         Me.lBoxLiinid.FormattingEnabled = True
-        Me.lBoxLiinid.Location = New System.Drawing.Point(786, 84)
+        Me.lBoxLiinid.Location = New System.Drawing.Point(231, 89)
         Me.lBoxLiinid.Name = "lBoxLiinid"
-        Me.lBoxLiinid.Size = New System.Drawing.Size(123, 264)
+        Me.lBoxLiinid.Size = New System.Drawing.Size(123, 368)
         Me.lBoxLiinid.TabIndex = 10
         Me.lBoxLiinid.Visible = False
-        '
-        'lBoxPeatused
-        '
-        Me.lBoxPeatused.FormattingEnabled = True
-        Me.lBoxPeatused.Location = New System.Drawing.Point(925, 84)
-        Me.lBoxPeatused.Name = "lBoxPeatused"
-        Me.lBoxPeatused.Size = New System.Drawing.Size(177, 264)
-        Me.lBoxPeatused.TabIndex = 11
-        Me.lBoxPeatused.Visible = False
-        '
-        'btnAB
-        '
-        Me.btnAB.Location = New System.Drawing.Point(925, 24)
-        Me.btnAB.Name = "btnAB"
-        Me.btnAB.Size = New System.Drawing.Size(177, 23)
-        Me.btnAB.TabIndex = 12
-        Me.btnAB.UseVisualStyleBackColor = True
-        Me.btnAB.Visible = False
-        '
-        'btnBA
-        '
-        Me.btnBA.Location = New System.Drawing.Point(925, 53)
-        Me.btnBA.Name = "btnBA"
-        Me.btnBA.Size = New System.Drawing.Size(177, 23)
-        Me.btnBA.TabIndex = 13
-        Me.btnBA.UseVisualStyleBackColor = True
-        Me.btnBA.Visible = False
-        '
-        'tBoxLink
-        '
-        Me.tBoxLink.Location = New System.Drawing.Point(784, 406)
-        Me.tBoxLink.Name = "tBoxLink"
-        Me.tBoxLink.Size = New System.Drawing.Size(318, 20)
-        Me.tBoxLink.TabIndex = 16
         '
         'Kaardirakendus
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1292, 595)
-        Me.Controls.Add(Me.tBoxLink)
-        Me.Controls.Add(Me.btnBA)
-        Me.Controls.Add(Me.btnAB)
-        Me.Controls.Add(Me.lBoxPeatused)
+        Me.ClientSize = New System.Drawing.Size(584, 595)
         Me.Controls.Add(Me.lBoxLiinid)
         Me.Controls.Add(Me.btnShowLines)
-        Me.Controls.Add(Me.btnAll)
-        Me.Controls.Add(Me.txtLat)
-        Me.Controls.Add(Me.txtLongName)
-        Me.Controls.Add(Me.btnDisplay)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.lblLongName)
-        Me.Controls.Add(Me.lblLat)
-        Me.Controls.Add(Me.btnChoose)
-        Me.Controls.Add(Me.GMapControl1)
         Me.Name = "Kaardirakendus"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents GMapControl1 As GMap.NET.WindowsForms.GMapControl
-    Friend WithEvents btnChoose As Button
-    Friend WithEvents lblLat As Label
-    Friend WithEvents lblLongName As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents btnDisplay As Button
-    Friend WithEvents txtLongName As TextBox
-    Friend WithEvents txtLat As TextBox
-    Friend WithEvents btnAll As Button
     Friend WithEvents btnShowLines As Button
     Friend WithEvents lBoxLiinid As ListBox
-    Friend WithEvents lBoxPeatused As ListBox
-    Friend WithEvents btnAB As Button
-    Friend WithEvents btnBA As Button
-    Friend WithEvents tBoxLink As TextBox
 End Class
--- KaartSWEPrj/Kaardirakendus.vb
+++ KaartSWEPrj/Kaardirakendus.vb
@@ -10,284 +10,31 @@ Imports MySql.Data.MySqlClient
 Imports System.Data.SQLite
 
 Public Class Kaardirakendus
-    Private Const CONN_STRING As String = "server=localhost;userid=root;password='1234';database=kaardidb"
-    'Private Const CONN_STRING As String = "server=b4jkjdcm9aadnnrqpgms-mysql.services.clever-cloud.com;userid=utkgha0in26npszi;password='AnYMkrGnd7P3qce4HZz7';database=b4jkjdcm9aadnnrqpgms"
-    Dim conn As MySqlConnection
-    Dim Suund As String
-    Dim LinkHalf As String
-    Dim SelectedLine As String
-    Dim SelectedStop As String
-    Dim SelectedStopId As String
+    Dim con As SQLiteConnection
+    Dim cmd As SQLiteCommand
 
-    'Algne versioon
-    Private Sub LoadLines()
-        conn = New MySqlConnection
-        conn.ConnectionString = CONN_STRING
-
-        Try
-            conn.Open()
-            Dim query As String = "select Line from liinid"
-            Dim cmd As New MySqlCommand(query, conn)
-            Dim reader As MySqlDataReader = cmd.ExecuteReader()
-            While reader.Read()
-                lBoxLiinid.Items.Add(reader.GetString(0))
-            End While
-            reader.Close()
-            conn.Close()
-        Catch ex As Exception
-            MsgBox(ex.Message)
-            conn.Close()
-        End Try
-    End Sub
-
-    Private Sub LoadLineStops(Suund As String)
-        lBoxPeatused.Items.Clear()
-        If lBoxPeatused.Visible = False Then
-            lBoxPeatused.Visible = True
-        End If
-        Dim asi As String = SelectedLine
-        conn = New MySqlConnection
-        conn.ConnectionString = CONN_STRING
-
-        Try
-            conn.Open()
-            Dim query As String = "select * from koikpeatused where liin = '" & asi & "' And suund ='" & Suund & "';"
-            Dim cmd As New MySqlCommand(query, conn)
-            Dim reader As MySqlDataReader = cmd.ExecuteReader()
-            While reader.Read()
-                lBoxPeatused.Items.Add(reader.GetString(2))
-            End While
-            reader.Close()
-            conn.Close()
-        Catch ex As Exception
-            MsgBox(ex.Message)
-            conn.Close()
-        End Try
-    End Sub
-
-    Private Sub LoadLineSuund(Liin As String)
-        btnAB.Visible = True
-        btnBA.Visible = True
-
-        btnAB.Text = Nothing
-        btnBA.Text = Nothing
-
-        conn = New MySqlConnection
-        conn.ConnectionString = CONN_STRING
-
-        Try
-            conn.Open()
-            Dim query As String = "select `a-b`,`b-a` from liinid where Line = '" & Liin & "';"
-            Dim cmd As New MySqlCommand(query, conn)
-            Dim reader As MySqlDataReader = cmd.ExecuteReader()
-
-            While reader.Read()
-                btnAB.Text = reader.GetString(0)
-                btnBA.Text = reader.GetString(1)
-            End While
-            reader.Close()
-            conn.Close()
-        Catch ex As Exception
-            MsgBox(ex.Message)
-            conn.Close()
-        End Try
-
-    End Sub
-
-    Private Sub LoadLineLink(Suund As String)
-        conn = New MySqlConnection
-        conn.ConnectionString = CONN_STRING
-        Dim SelectElement As String
-        If Suund = "a-b" Then
-            SelectElement = "link_a-b"
-        ElseIf Suund = "b-a" Then
-            SelectElement = "link_b-a"
-        Else
-            SelectElement = Nothing
-            Exit Sub
-        End If
-        Try
-            conn.Open()
-            Dim query As String = "select `" & SelectElement & "` from liinid Where line = '" & SelectedLine & "';"
-            'MsgBox(query)
-            Dim cmd As New MySqlCommand(query, conn)
-            Dim reader As MySqlDataReader = cmd.ExecuteReader()
-
-            While reader.Read()
-                LinkHalf = reader.GetString(0)
-
-            End While
-            reader.Close()
-            conn.Close()
-        Catch ex As Exception
-            MsgBox(ex.Message)
-            conn.Close()
-        End Try
-    End Sub
-
-    Private Sub Kaardirakendus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        GMapControl1.MapProvider = GoogleMapProvider.Instance
-        GMaps.Instance.Mode = AccessMode.ServerAndCache
-        GMapControl1.ShowCenter = False
-
-        GMapControl1.Position = New GMap.NET.PointLatLng(59.4380930599551, 24.7590637207031)
-        GMapControl1.MinZoom = 5
-        GMapControl1.MaxZoom = 100
-        GMapControl1.Zoom = 10
-    End Sub
-
-    Private Sub GMapControl1_OnMapClick(sender As Object, e As MouseEventArgs) Handles GMapControl1.OnMapClick
-        ' Get the latitude and longitude of the clicked point
-        Dim lat As Double = GMapControl1.FromLocalToLatLng(e.X, e.Y).Lat
-        Dim lng As Double = GMapControl1.FromLocalToLatLng(e.X, e.Y).Lng
-        If (choose) Then
-            btnChoose.Text = "Name"
-            lblLongName.Text = "Longitude"
-            lblLat.Visible = True
-            txtLat.Visible = True
-            choose = False
-        End If
-        txtLongName.Text = lng
-        txtLat.Text = lat
-    End Sub
-
-    Dim choose As Boolean = True
-
-    Private Sub btnChoose_Click(sender As Object, e As EventArgs) Handles btnChoose.Click
-        If (choose) Then
-            btnChoose.Text = "Name"
-            lblLongName.Text = "Longitude"
-            txtLongName.Text = ""
-            lblLat.Visible = True
-            txtLat.Visible = True
-            choose = False
-        Else
-            btnChoose.Text = "Coordinates"
-            lblLongName.Text = "Name"
-            txtLongName.Text = ""
-            txtLat.Text = ""
-            lblLat.Visible = False
-            txtLat.Visible = False
-            choose = True
-        End If
-    End Sub
-
-    Private lastMarker As GMapMarker
-
-    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
-        If (choose) Then
-
-        Else
-            ' Clear all markers from the map
-            GMapControl1.Overlays.Clear()
-
-            ' Get the longitude and latitude coordinates from the textboxes
-            Dim longCoord As Double = CDbl(Val(txtLat.Text))
-            Dim latCoord As Double = CDbl(Val(txtLongName.Text))
-
-            ' Set the position of the map control to the specified coordinates
-            GMapControl1.ShowCenter = False
-            GMapControl1.Position = New GMap.NET.PointLatLng(longCoord, latCoord)
-
-            ' Create a new marker and add it to the map
-            Dim markers As GMapOverlay = New GMapOverlay("polygons")
-            Dim marker As GMapMarker = New GMarkerGoogle(GMapControl1.Position, GMarkerGoogleType.blue_dot)
-            markers.Markers.Add(marker)
-            GMapControl1.Overlays.Add(markers)
-
-            ' Set some zoom properties and tooltip mode for the marker
-            GMapControl1.MinZoom = 5
-            GMapControl1.MaxZoom = 100
-            GMapControl1.Zoom = 14
-            marker.ToolTipMode = MarkerTooltipMode.OnMouseOver
-        End If
-    End Sub
-
-    Private Sub btnAll_Click(sender As Object, e As EventArgs) Handles btnAll.Click
-        ' Clear all markers from the map
-        GMapControl1.Overlays.Clear()
-
-        ' Download the stops.txt file
-        Dim client As WebClient = New WebClient()
-        Dim data As Stream = client.OpenRead("https://transport.tallinn.ee/data/stops.txt")
-        Dim reader As New StreamReader(data)
-        Dim line As String
-        Dim fields As String()
-        Dim lat As Double
-        Dim lng As Double
-
-        GMapControl1.ShowCenter = False
-        Dim markers As GMapOverlay = New GMapOverlay("markers")
-        While Not reader.EndOfStream
-            line = reader.ReadLine()
-            fields = line.Split(";")
-            If fields.Length >= 4 AndAlso Integer.TryParse(fields(2), Nothing) AndAlso Integer.TryParse(fields(3), Nothing) Then
-                lat = CDbl(fields(2)) / 100000
-                lng = CDbl(fields(3)) / 100000
-                Dim busStop As PointLatLng = New GMap.NET.PointLatLng(lat, lng)
-                Dim marker As GMapMarker = New GMarkerGoogle(busStop, GMarkerGoogleType.red_small)
-                markers.Markers.Add(marker)
-                'Console.WriteLine("Bus Stop at Latitude: " & busStop.Lat.ToString() & ", Longitude: " & busStop.Lng.ToString())
-            End If
-        End While
-
-        ' Add the markers overlay to the map
-        GMapControl1.Overlays.Add(markers)
-        GMapControl1.Refresh()
-    End Sub
 
     Private Sub btnShowLines_Click(sender As Object, e As EventArgs) Handles btnShowLines.Click
-        If lBoxPeatused.Visible = True Then
-            Exit Sub
-        End If
-        lBoxLiinid.Visible = True
-        LoadLines()
-    End Sub
-
-    Private Sub btnAB_Click(sender As Object, e As EventArgs) Handles btnAB.Click
-        Suund = "a-b"
-        LoadLineLink(Suund)
-        LoadLineStops(Suund)
-    End Sub
-
-    Private Sub btnBA_Click(sender As Object, e As EventArgs) Handles btnBA.Click
-        Suund = "b-a"
-        LoadLineLink(Suund)
-        LoadLineStops(Suund)
-    End Sub
-
-    Private Sub lBoxLiinid_SelectedValueChanged(sender As Object, e As EventArgs) Handles lBoxLiinid.SelectedValueChanged
-        lBoxPeatused.Items.Clear()
-        SelectedLine = lBoxLiinid.SelectedItem
-        LoadLineSuund(SelectedLine)
-    End Sub
-
-    Private Function lBoxPeatused_SelectedValueChanged(sender As Object, e As EventArgs) Handles lBoxPeatused.SelectedIndexChanged
-        SelectedStop = lBoxPeatused.SelectedItem
-        'MsgBox(SelectedLine & " " & SelectedStop & " " & Suund & " " & LinkHalf)
-        conn = New MySqlConnection
-        conn.ConnectionString = CONN_STRING
-        Dim PeatuseID As String = Nothing
 
         Try
-            conn.Open()
-            Dim query As String = "select PeatuseID from koikpeatused where PeatuseNimi = '" & SelectedStop & "' And Liin ='" & SelectedLine & "';"
-            Dim cmd As New MySqlCommand(query, conn)
-            Dim reader As MySqlDataReader = cmd.ExecuteReader()
+            con = New SQLiteConnection("Data Source=C://Users//S11M//Desktop//sqliteDB//mapsdb.db;Version=3;")
+            con.Open()
+            cmd = New SQLiteCommand("SELECT Line FROM liinid", con)
+            Dim reader As SQLiteDataReader = cmd.ExecuteReader()
+
+            lBoxLiinid.Visible = True
+
             While reader.Read()
-                PeatuseID = reader.GetString(0)
+                Dim line As String = reader.GetString(0)
+                lBoxLiinid.Items.Add(line)
+
             End While
-            reader.Close()
-            conn.Close()
+
         Catch ex As Exception
-            MsgBox(ex.Message)
-            conn.Close()
+            MsgBox(ex)
+        Finally
+            con.Close()
         End Try
-        Dim LinkFull As String = LinkHalf & "/" & PeatuseID
-        tBoxLink.Visible = True
-        tBoxLink.Text = LinkFull
-        Return LinkFull
-    End Function
+    End Sub
 
 End Class
