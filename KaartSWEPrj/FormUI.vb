8b5659119fb979a8397b2aecf214b0fac2e74da8
Rannar Randmäe <rannra@ttu.ee>
2023-04-17 21:02:52 +0300

    Removed comments

--- KaartSWEPrj/FormUI.vb
+++ KaartSWEPrj/FormUI.vb
@@ -9,7 +9,6 @@ Imports System.Windows.Forms.VisualStyles
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class Kaardirakendus
-    'Dim choose As Boolean = True
     Public Property startPoint As New PointLatLng
     Public Property endPoint As New PointLatLng
 
@@ -54,72 +53,4 @@ Public Class Kaardirakendus
         btnRoute.Enabled = False
         UCtrlMapViewer1.clearRoute()
     End Sub
-
-
-    'Private Sub GMapControl1_OnMapClick(sender As Object, e As MouseEventArgs) Handles GMapControl1.OnMapClick
-    ' Get the latitude and longitude of the clicked point
-    'Dim lat As Double = GMapControl1.FromLocalToLatLng(e.X, e.Y).Lat
-    'Dim lng As Double = GMapControl1.FromLocalToLatLng(e.X, e.Y).Lng
-    'If (choose) Then
-    '       btnChoose.Text = "Name"
-    '      lblLongName.Text = "Longitude"
-    '     lblLat.Visible = True
-    '    txtLat.Visible = True
-    '   choose = False
-    'End If
-    '   txtLongName.Text = lng
-    '  txtLat.Text = lat
-    'End Sub
-
-
-    'Private Sub btnChoose_Click(sender As Object, e As EventArgs) Handles btnChoose.Click
-    'If (choose) Then
-    '       btnChoose.Text = "Name"
-    '      lblLongName.Text = "Longitude"
-    '     txtLongName.Text = ""
-    '    lblLat.Visible = True
-    '   txtLat.Visible = True
-    '  choose = False
-    'Else
-    '       btnChoose.Text = "Coordinates"
-    '      lblLongName.Text = "Name"
-    '     txtLongName.Text = ""
-    '    txtLat.Text = ""
-    '   lblLat.Visible = False
-    '  txtLat.Visible = False
-    ' choose = True
-    'End If
-    'End Sub
-
-    'Private lastMarker As GMapMarker
-
-    'Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
-    'If (choose) Then
-
-    'Else
-    ' Clear all markers from the map
-    '       GMapControl1.Overlays.Clear()
-
-    ' Get the longitude and latitude coordinates from the textboxes
-    'Dim longCoord As Double = CDbl(Val(txtLat.Text))
-    'Dim latCoord As Double = CDbl(Val(txtLongName.Text))
-
-    ' Set the position of the map control to the specified coordinates
-    '       GMapControl1.ShowCenter = False
-    '      GMapControl1.Position = New GMap.NET.PointLatLng(longCoord, latCoord)
-
-    ' Create a new marker and add it to the map
-    'Dim markers As GMapOverlay = New GMapOverlay("polygons")
-    'Dim marker As GMapMarker = New GMarkerGoogle(GMapControl1.Position, GMarkerGoogleType.blue_dot)
-    '       markers.Markers.Add(marker)
-    '      GMapControl1.Overlays.Add(markers)
-
-    ' Set some zoom properties and tooltip mode for the marker
-    '     GMapControl1.MinZoom = 5
-    '    GMapControl1.MaxZoom = 100
-    '   GMapControl1.Zoom = 14
-    '  marker.ToolTipMode = MarkerTooltipMode.OnMouseOver
-    'End If
-    'End Sub
-
 End Class
