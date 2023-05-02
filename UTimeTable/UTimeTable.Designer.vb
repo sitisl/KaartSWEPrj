cadbf6a1b2aa2d959eb478fc52e3fbf4d4fa5666
Markus Visnapuu <mvisna@ttu.ee>
2023-05-02 11:58:45 +0300

    Timetable data from SQL database and realtime data from request

--- UTimeTable/UTimeTable.Designer.vb
+++ UTimeTable/UTimeTable.Designer.vb
@@ -27,7 +27,6 @@ Partial Class UTimeTable
         Me.btnAB = New System.Windows.Forms.Button()
         Me.lBoxPeatused = New System.Windows.Forms.ListBox()
         Me.lBoxLiinid = New System.Windows.Forms.ListBox()
-        Me.rtbAjad = New System.Windows.Forms.RichTextBox()
         Me.lblLiinid = New System.Windows.Forms.Label()
         Me.lblPeatused = New System.Windows.Forms.Label()
         Me.lblAjad = New System.Windows.Forms.Label()
@@ -35,16 +34,19 @@ Partial Class UTimeTable
         Me.lblAbi = New System.Windows.Forms.Label()
         Me.btnShowLines = New System.Windows.Forms.Button()
         Me.btnShowStops = New System.Windows.Forms.Button()
+        Me.rtbAjad = New System.Windows.Forms.RichTextBox()
+        Me.btnDay1 = New System.Windows.Forms.Button()
+        Me.btnDay2 = New System.Windows.Forms.Button()
+        Me.btnDay3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'lBoxRealTime
         '
         Me.lBoxRealTime.FormattingEnabled = True
-        Me.lBoxRealTime.Location = New System.Drawing.Point(297, 388)
+        Me.lBoxRealTime.Location = New System.Drawing.Point(287, 388)
         Me.lBoxRealTime.Name = "lBoxRealTime"
-        Me.lBoxRealTime.Size = New System.Drawing.Size(110, 56)
+        Me.lBoxRealTime.Size = New System.Drawing.Size(192, 56)
         Me.lBoxRealTime.TabIndex = 29
-        Me.lBoxRealTime.Visible = False
         '
         'btnBA
         '
@@ -53,7 +55,6 @@ Partial Class UTimeTable
         Me.btnBA.Size = New System.Drawing.Size(335, 23)
         Me.btnBA.TabIndex = 25
         Me.btnBA.UseVisualStyleBackColor = True
-        Me.btnBA.Visible = False
         '
         'btnAB
         '
@@ -62,16 +63,14 @@ Partial Class UTimeTable
         Me.btnAB.Size = New System.Drawing.Size(335, 23)
         Me.btnAB.TabIndex = 24
         Me.btnAB.UseVisualStyleBackColor = True
-        Me.btnAB.Visible = False
         '
         'lBoxPeatused
         '
         Me.lBoxPeatused.FormattingEnabled = True
         Me.lBoxPeatused.Location = New System.Drawing.Point(109, 76)
         Me.lBoxPeatused.Name = "lBoxPeatused"
-        Me.lBoxPeatused.Size = New System.Drawing.Size(130, 368)
+        Me.lBoxPeatused.Size = New System.Drawing.Size(172, 368)
         Me.lBoxPeatused.TabIndex = 23
-        Me.lBoxPeatused.Visible = False
         '
         'lBoxLiinid
         '
@@ -80,19 +79,6 @@ Partial Class UTimeTable
         Me.lBoxLiinid.Name = "lBoxLiinid"
         Me.lBoxLiinid.Size = New System.Drawing.Size(100, 368)
         Me.lBoxLiinid.TabIndex = 22
-        Me.lBoxLiinid.Visible = False
-        '
-        'rtbAjad
-        '
-        Me.rtbAjad.BackColor = System.Drawing.SystemColors.Window
-        Me.rtbAjad.DetectUrls = False
-        Me.rtbAjad.Location = New System.Drawing.Point(245, 76)
-        Me.rtbAjad.Name = "rtbAjad"
-        Me.rtbAjad.ReadOnly = True
-        Me.rtbAjad.Size = New System.Drawing.Size(234, 264)
-        Me.rtbAjad.TabIndex = 30
-        Me.rtbAjad.Text = ""
-        Me.rtbAjad.Visible = False
         '
         'lblLiinid
         '
@@ -102,42 +88,38 @@ Partial Class UTimeTable
         Me.lblLiinid.Size = New System.Drawing.Size(31, 13)
         Me.lblLiinid.TabIndex = 31
         Me.lblLiinid.Text = "Liinid"
-        Me.lblLiinid.Visible = False
         '
         'lblPeatused
         '
         Me.lblPeatused.AutoSize = True
-        Me.lblPeatused.Location = New System.Drawing.Point(141, 60)
+        Me.lblPeatused.Location = New System.Drawing.Point(162, 60)
         Me.lblPeatused.Name = "lblPeatused"
         Me.lblPeatused.Size = New System.Drawing.Size(52, 13)
         Me.lblPeatused.TabIndex = 32
         Me.lblPeatused.Text = "Peatused"
-        Me.lblPeatused.Visible = False
         '
         'lblAjad
         '
         Me.lblAjad.AutoSize = True
-        Me.lblAjad.Location = New System.Drawing.Point(349, 60)
+        Me.lblAjad.Location = New System.Drawing.Point(370, 60)
         Me.lblAjad.Name = "lblAjad"
         Me.lblAjad.Size = New System.Drawing.Size(28, 13)
         Me.lblAjad.TabIndex = 33
         Me.lblAjad.Text = "Ajad"
-        Me.lblAjad.Visible = False
         '
         'lblReaalajad
         '
         Me.lblReaalajad.AutoSize = True
-        Me.lblReaalajad.Location = New System.Drawing.Point(322, 372)
+        Me.lblReaalajad.Location = New System.Drawing.Point(352, 372)
         Me.lblReaalajad.Name = "lblReaalajad"
         Me.lblReaalajad.Size = New System.Drawing.Size(55, 13)
         Me.lblReaalajad.TabIndex = 34
         Me.lblReaalajad.Text = "Reaalajad"
-        Me.lblReaalajad.Visible = False
         '
         'lblAbi
         '
         Me.lblAbi.AutoSize = True
-        Me.lblAbi.Location = New System.Drawing.Point(284, 343)
+        Me.lblAbi.Location = New System.Drawing.Point(310, 343)
         Me.lblAbi.Name = "lblAbi"
         Me.lblAbi.Size = New System.Drawing.Size(142, 13)
         Me.lblAbi.TabIndex = 35
@@ -162,16 +144,55 @@ Partial Class UTimeTable
         Me.btnShowStops.Text = "Show all stops"
         Me.btnShowStops.UseVisualStyleBackColor = True
         '
+        'rtbAjad
+        '
+        Me.rtbAjad.Location = New System.Drawing.Point(287, 105)
+        Me.rtbAjad.Name = "rtbAjad"
+        Me.rtbAjad.ReadOnly = True
+        Me.rtbAjad.Size = New System.Drawing.Size(192, 235)
+        Me.rtbAjad.TabIndex = 37
+        Me.rtbAjad.Text = ""
+        '
+        'btnDay1
+        '
+        Me.btnDay1.Location = New System.Drawing.Point(287, 76)
+        Me.btnDay1.Name = "btnDay1"
+        Me.btnDay1.Size = New System.Drawing.Size(64, 23)
+        Me.btnDay1.TabIndex = 38
+        Me.btnDay1.Text = "Tööpäev"
+        Me.btnDay1.UseVisualStyleBackColor = True
+        '
+        'btnDay2
+        '
+        Me.btnDay2.Location = New System.Drawing.Point(351, 76)
+        Me.btnDay2.Name = "btnDay2"
+        Me.btnDay2.Size = New System.Drawing.Size(64, 23)
+        Me.btnDay2.TabIndex = 39
+        Me.btnDay2.Text = "Laupäev"
+        Me.btnDay2.UseVisualStyleBackColor = True
+        '
+        'btnDay3
+        '
+        Me.btnDay3.Location = New System.Drawing.Point(415, 76)
+        Me.btnDay3.Name = "btnDay3"
+        Me.btnDay3.Size = New System.Drawing.Size(64, 23)
+        Me.btnDay3.TabIndex = 40
+        Me.btnDay3.Text = "Pühapäev"
+        Me.btnDay3.UseVisualStyleBackColor = True
+        '
         'UTimeTable
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.btnDay3)
+        Me.Controls.Add(Me.btnDay2)
+        Me.Controls.Add(Me.btnDay1)
+        Me.Controls.Add(Me.rtbAjad)
         Me.Controls.Add(Me.lblAbi)
         Me.Controls.Add(Me.lblReaalajad)
         Me.Controls.Add(Me.lblAjad)
         Me.Controls.Add(Me.lblPeatused)
         Me.Controls.Add(Me.lblLiinid)
-        Me.Controls.Add(Me.rtbAjad)
         Me.Controls.Add(Me.lBoxRealTime)
         Me.Controls.Add(Me.btnShowStops)
         Me.Controls.Add(Me.btnBA)
@@ -191,7 +212,6 @@ Partial Class UTimeTable
     Friend WithEvents btnAB As Button
     Friend WithEvents lBoxPeatused As ListBox
     Friend WithEvents lBoxLiinid As ListBox
-    Friend WithEvents rtbAjad As RichTextBox
     Friend WithEvents lblLiinid As Label
     Friend WithEvents lblPeatused As Label
     Friend WithEvents lblAjad As Label
@@ -199,4 +219,8 @@ Partial Class UTimeTable
     Friend WithEvents lblAbi As Label
     Friend WithEvents btnShowLines As Button
     Friend WithEvents btnShowStops As Button
+    Friend WithEvents rtbAjad As RichTextBox
+    Friend WithEvents btnDay1 As Button
+    Friend WithEvents btnDay2 As Button
+    Friend WithEvents btnDay3 As Button
 End Class
